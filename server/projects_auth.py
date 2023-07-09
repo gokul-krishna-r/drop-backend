b3b9a51e3323ea6a6773c560a0e617ff8b68c2ab
Gokul <gokulkrishna.gk32@gmail.com>
2023-07-09 23:27:01 +0530

    Update projects_auth.py

--- server/projects_auth.py
+++ server/projects_auth.py
@@ -55,7 +55,6 @@ async def create_project(background_tasks: BackgroundTasks,envText: str = Body(d
     user_id = user["_id"]
     background_tasks.add_task(create_project_task, envText, projects,user_id)
 
-    create_project_task(envText, projects, user_id)
 
 
 
